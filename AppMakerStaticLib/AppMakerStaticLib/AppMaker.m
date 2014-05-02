5d63ef01e9626af2a74e2a70ae989e4c6a094362
Christian Lao <christian@romino.lan>
2014-05-02 19:32:17 +0200

    Now it registers always for push

--- AppMakerStaticLib/AppMakerStaticLib/AppMaker.m
+++ AppMakerStaticLib/AppMakerStaticLib/AppMaker.m
@@ -741,10 +741,10 @@ static id appMaker = nil;
 	{
 		[self registerAPNToken:self.lastAPNToken forNotificationsEnabled:NO];
 	}
-	else if (self.serverPushEnabled && !currentStatusDenied)
-	{
-		return;
-	}
+//	else if (self.serverPushEnabled && !currentStatusDenied)
+//	{
+//		return;
+//	}
 	else
 	{
 		[[UIApplication sharedApplication] registerForRemoteNotificationTypes:UIRemoteNotificationTypeSound|UIRemoteNotificationTypeAlert];
@@ -812,7 +812,7 @@ static id appMaker = nil;
 		[request setHTTPMethod:@"POST"];
 		[request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
 		[request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-		[request setValue:[NSString stringWithFormat:@"%d", [jsonData length]] forHTTPHeaderField:@"Content-Length"];
+		[request setValue:[NSString stringWithFormat:@"%lu", (unsigned long)[jsonData length]] forHTTPHeaderField:@"Content-Length"];
 		[request setHTTPBody:jsonData];
 		
 #ifdef DEBUG
